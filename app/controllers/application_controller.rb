d16fede9d64157a5eea5252604d63f262a1c02a4
SmallNic <nas231@nyu.edu>
2016-04-08 14:58:40 -0400

    initial changes to bus times app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,48 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def lookupWeather(user_zip)
+    zip = user_zip || "20011"
+    zipcodeLookupURL = "http://api.wunderground.com/api/e00b469c839d4b27/geolookup/q/"+zip+".json"
+    weatherData = HTTParty.get(zipcodeLookupURL)
+    state = weatherData["location"]["state"]
+    city = weatherData["location"]["city"]
+    cityLookupURL = "http://api.wunderground.com/api/e00b469c839d4b27/conditions/q/" + state + "/" + city + ".json"
+    cityData = HTTParty.get(cityLookupURL)
+    return cityData["current_observation"]["temp_f"]
+  end
+
+  def get_predictions(stop_id, bus_name)
+    next_bus_data = HTTParty.get("https://api.wmata.com/NextBusService.svc/json/jPredictions?api_key=ff3f1616de1b4f49ab93a499b1bd4bb6&StopID=#{stop_id}")
+    stop_name = next_bus_data["StopName"]
+
+    next_buses_on_route=[]
+    other_buses_on_route=[]
+    next_bus_data["Predictions"].each do |prediction|
+      if prediction["RouteID"] == bus_name
+        direction_name = prediction["DirectionText"]
+        minutes_away = prediction["Minutes"]
+        next_buses_on_route << {route_id:bus_name, stop_id:stop_id, stop_name:stop_name, direction_name:direction_name, minutes_away:minutes_away}
+      else
+        direction_name = prediction["DirectionText"]
+        minutes_away = prediction["Minutes"]
+        other_buses_on_route << {route_id:prediction["RouteID"], stop_id:stop_id, stop_name:stop_name, direction_name:direction_name, minutes_away:minutes_away}
+      end
+    end
+
+    return [next_buses_on_route, other_buses_on_route]
+  end
+
+  def get_stop_id(stop_name)
+    bus_data = HTTParty.get("https://api.wmata.com/Bus.svc/json/jStops?Lat&Lon&Radius&api_key=ff3f1616de1b4f49ab93a499b1bd4bb6")
+
+    bus_data["Stops"].each do |stop|
+      if stop["Name"] == stop_name
+        return stop["StopID"]
+      end
+    end
+  end
+
+
 end
--- app/controllers/fave_buses_controller.rb
+++ app/controllers/fave_buses_controller.rb
@@ -1,4 +1,6 @@
 class FaveBusesController < ApplicationController
+  before_action :set_temp
+
 
   def get_list_of_stops_for_bus(chosen_bus)
     all_bus_data = HTTParty.get("https://api.wmata.com/Bus.svc/json/jStops?Lat&Lon&Radius&api_key=ff3f1616de1b4f49ab93a499b1bd4bb6")
@@ -15,47 +17,7 @@ class FaveBusesController < ApplicationController
     return stops_for_chosen_bus.uniq
   end
 
-  def get_predictions(stop_id, bus_name)
-    next_bus_data = HTTParty.get("https://api.wmata.com/NextBusService.svc/json/jPredictions?api_key=ff3f1616de1b4f49ab93a499b1bd4bb6&StopID=#{stop_id}")
-    stop_name = next_bus_data["StopName"]
-
-    next_buses_on_route=[]
-    other_buses_on_route=[]
-    next_bus_data["Predictions"].each do |prediction|
-      if prediction["RouteID"] == bus_name
-        direction_name = prediction["DirectionText"]
-        minutes_away = prediction["Minutes"]
-        next_buses_on_route << {route_id:bus_name, stop_id:stop_id, stop_name:stop_name, direction_name:direction_name, minutes_away:minutes_away}
-      else
-        direction_name = prediction["DirectionText"]
-        minutes_away = prediction["Minutes"]
-        other_buses_on_route << {route_id:prediction["RouteID"], stop_id:stop_id, stop_name:stop_name, direction_name:direction_name, minutes_away:minutes_away}
-      end
-    end
-
-    return [next_buses_on_route, other_buses_on_route]
-  end
-
-
-  def get_stop_id(stop_name)
-    bus_data = HTTParty.get("https://api.wmata.com/Bus.svc/json/jStops?Lat&Lon&Radius&api_key=ff3f1616de1b4f49ab93a499b1bd4bb6")
-
-    bus_data["Stops"].each do |stop|
-      if stop["Name"] == stop_name
-        return stop["StopID"]
-      end
-    end
-  end
-
   def index
-    zipcodeLookupURL = "http://api.wunderground.com/api/e00b469c839d4b27/geolookup/q/20011.json"
-    weatherData = HTTParty.get(zipcodeLookupURL)
-    state = weatherData["location"]["state"]
-    city = weatherData["location"]["city"]
-    cityLookupURL = "http://api.wunderground.com/api/e00b469c839d4b27/conditions/q/" + state + "/" + city + ".json"
-    cityData = HTTParty.get(cityLookupURL)
-    @temp = cityData["current_observation"]["temp_f"]
-
     @fave_bus_routes = []
     FaveBus.all.each do |fave_bus|
       @fave_bus_routes << get_predictions(fave_bus.stop_id, fave_bus.route_id).first
@@ -91,5 +53,12 @@ class FaveBusesController < ApplicationController
     end
     redirect_to ('/')
   end
+
+  private
+
+  def set_temp
+    @temp = lookupWeather(nil)
+  end
+
 
 end
